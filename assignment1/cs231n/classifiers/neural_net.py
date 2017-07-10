c2d725fe3cb04e0e192e57c561da82d61c7d1557
Rae Jeong <raychanjeong@gmail.com>
2017-07-10 10:00:58 -0700

    finished assignment 1

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -4,6 +4,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from past.builtins import xrange
 
+def f_hinge(X):
+  X[X<0] = 0
+  return X
+
+def df_hinge(X, dF):
+  X[X<0] = 0
+  X[X>0] = 1
+  return dF*X
+
+def f_softmax(X, y):
+  X -= np.amax(X,axis=1)[:,np.newaxis]
+  return -X[np.arange(X.shape[0]),y] + np.log(np.sum(np.exp(X), axis=1))
+
+def df_softmax(X, y, dF):
+  dX = np.exp(X)/np.sum(np.exp(X),axis=1)[:,np.newaxis]
+  dX[np.arange(X.shape[0]),y] += -1
+  return dX*dF
+
+def df_matrix_multiplication(A,B,dF):
+  dA = dF.dot(B.T) #.T gives the transpose of the matrix
+  dB = A.T.dot(dF)
+  return dA, dB
+
 class TwoLayerNet(object):
   """
   A two-layer fully-connected neural network. The net has an input dimension of
@@ -76,7 +99,14 @@ class TwoLayerNet(object):
     # Store the result in the scores variable, which should be an array of      #
     # shape (N, C).                                                             #
     #############################################################################
-    pass
+    # scores = hinge_f(X.dot(W1)+b1).dot(W2) + b2
+    A1 = X.dot(W1)
+    Ab1 = A1 + b1
+    Z1 = f_hinge(Ab1)
+    A2 = Z1.dot(W2)
+    Ab2 = A2 + b2
+
+    scores = Ab2
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -93,19 +123,59 @@ class TwoLayerNet(object):
     # in the variable loss, which should be a scalar. Use the Softmax           #
     # classifier loss.                                                          #
     #############################################################################
-    pass
+    # S = scores.copy()
+    # S -= np.amax(S,axis=1)[:,np.newaxis]
+    # Z2 = -S[np.arange(N),y]+np.log(np.sum(np.exp(S),axis=1))
+    # loss = np.sum(Z2)
+    # loss /= N
+    # loss += reg * (np.sum(W1*W1) + np.sum(W2*W2))
+    Z2 = f_softmax(Ab2,y)
+    loss = np.sum(Z2)/N + reg * (np.sum(W1*W1) + np.sum(W2*W2))
+
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
 
     # Backward pass: compute gradients
     grads = {}
+
     #############################################################################
     # TODO: Compute the backward pass, computing the derivatives of the weights #
     # and biases. Store the results in the grads dictionary. For example,       #
     # grads['W1'] should store the gradient on W1, and be a matrix of same size #
     #############################################################################
-    pass
+
+    d_loss = 1
+    d_Z2 = d_loss/N
+    d_Ab2 = df_softmax(Ab2,y,d_Z2)
+    grads['b2'] = np.sum(d_Ab2,axis=0)
+    d_A2 = d_Ab2
+    d_Z1, grads['W2'] = df_matrix_multiplication(Z1, W2, d_A2)
+    d_Ab1 = df_hinge(Ab1, d_Z1)
+    grads['b1'] = np.sum(d_Ab1,axis=0)
+    d_A1 = d_Ab1
+    d_X, grads['W1'] = df_matrix_multiplication(X, W1, d_A1)
+    grads['W1'] += 2*reg*W1
+    grads['W2'] += 2*reg*W2
+    # dZ2dAb2 = (1-Z2)*Z2
+    # dAb2db2 = np.eye(dZ2dAb2.T.shape[0])
+    # dA2dW2 = Z1
+    # dAb2dA2 = np.eye(dA2dW2.T.shape[0],dA2dW2.T.shape[1])
+    # dA2dZ1 = W2
+    # Ab1C = Ab1.copy()
+    # Ab1C[Ab1C>0] = 1
+    # Ab1C[Ab1C<0] = 0
+    # dZ1dAb1 = Ab1C
+    # dAb1db1 = np.eye(dZ1dAb1.T.shape[0],dZ1dAb1.T.shape[1])
+    # dAb1dA1 = np.eye(dZ1dAb1.T.shape[0],dZ1dAb1.T.shape[1])
+    # dA1dW1 = X
+    # dA1dX = W1
+
+    # grads['b2'] = dAb2db2.dot(dZ2dAb2)
+    # grads['W2'] = dA2dW2.dot(dAb2dA2).dot(dZ2dAb2)
+    # grads['b1'] = dAb1db1.dot(dZ1dAb1).dot(dA2dZ1).dot(dAb2dA2).dot(dZ2dAb2)
+    # grads['W1'] = dA1dW1.dot(dAb1dA1).dot(dZ1dAb1).dot(dA2dZ1).dot(dAb2dA2).dot(dZ2dAb2)
+
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -149,7 +219,9 @@ class TwoLayerNet(object):
       # TODO: Create a random minibatch of training data and labels, storing  #
       # them in X_batch and y_batch respectively.                             #
       #########################################################################
-      pass
+      sample_list = np.random.choice(num_train,batch_size)
+      X_batch = X[sample_list]
+      y_batch = y[sample_list]
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
@@ -158,13 +230,27 @@ class TwoLayerNet(object):
       loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
       loss_history.append(loss)
 
+      drop_out = -0.001
+      W1_mask = np.random.randn(self.params['W1'].shape[0],self.params['W1'].shape[1])
+      W2_mask = np.random.randn(self.params['W2'].shape[0],self.params['W2'].shape[1])
+      W1_mask[W1_mask<=drop_out] = 0
+      W1_mask[W1_mask>drop_out] = 1
+      W2_mask[W2_mask<=drop_out] = 0
+      W2_mask[W2_mask>drop_out] = 1
+
+      W1_mask = np.ones((self.params['W1'].shape[0],self.params['W1'].shape[1]))
+      W2_mask = np.ones((self.params['W2'].shape[0],self.params['W2'].shape[1]))
+
       #########################################################################
       # TODO: Use the gradients in the grads dictionary to update the         #
       # parameters of the network (stored in the dictionary self.params)      #
       # using stochastic gradient descent. You'll need to use the gradients   #
       # stored in the grads dictionary defined above.                         #
       #########################################################################
-      pass
+      self.params['W1'] -= W1_mask*learning_rate*grads['W1']
+      self.params['W2'] -= W2_mask*learning_rate*grads['W2']
+      self.params['b1'] -= learning_rate*grads['b1']
+      self.params['b2'] -= learning_rate*grads['b2']
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
@@ -174,7 +260,7 @@ class TwoLayerNet(object):
 
       # Every epoch, check train and val accuracy and decay learning rate.
       if it % iterations_per_epoch == 0:
-        # Check accuracy
+        # Check accuracy  
         train_acc = (self.predict(X_batch) == y_batch).mean()
         val_acc = (self.predict(X_val) == y_val).mean()
         train_acc_history.append(train_acc)
@@ -209,11 +295,9 @@ class TwoLayerNet(object):
     ###########################################################################
     # TODO: Implement this function; it should be VERY simple!                #
     ###########################################################################
-    pass
+    y_pred = np.argmax(f_hinge(X.dot(self.params['W1'])+self.params['b1']).dot(self.params['W2'])+self.params['b2'],axis=1)
     ###########################################################################
     #                              END OF YOUR CODE                           #
     ###########################################################################
 
     return y_pred
-
-
